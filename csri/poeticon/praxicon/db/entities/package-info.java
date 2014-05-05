26917af8b35fbe8f1bf130dada9d5375451fd867
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2014-05-05 20:44:35 +0300

    Deleted generated xsd files.

--- csri/poeticon/praxicon/db/entities/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.csri.gr", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package csri.poeticon.praxicon.db.entities;
