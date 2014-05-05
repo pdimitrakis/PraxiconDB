016d3645e5fcddcb3ef543951515a3b60497a18b
Dimitris Mavroeidis <dmavroeidis@csri.gr>
2014-05-05 10:56:11 +0300

    Changes for correct XSD generation.

--- csri/poeticon/praxicon/GeneratePraxiconXSD.java
+++ csri/poeticon/praxicon/GeneratePraxiconXSD.java
@@ -7,31 +7,58 @@
 package csri.poeticon.praxicon;
 
 import csri.poeticon.praxicon.db.entities.Concept;
+import csri.poeticon.praxicon.db.entities.CollectionOfConcepts;
+import csri.poeticon.praxicon.db.entities.LanguageRepresentation;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.text.Document;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.SchemaOutputResolver;
+import javax.xml.transform.Result;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.stream.StreamResult;
+import org.apache.xerces.dom.DocumentImpl;
+import org.hibernate.internal.jaxb.mapping.orm.ObjectFactory;
 
 /**
  *
  * @author dmavroeidis
  */
 public class GeneratePraxiconXSD {
-    public static SchemaOutputResolver scoure;
-    JAXBContext jaxbContext;
+//    public static SchemaOutputResolver scoure;
+//    JAXBContext jaxbContext;
     
-    public GeneratePraxiconXSD() throws JAXBException 
+    public GeneratePraxiconXSD(String suggestedSchemaName) throws JAXBException, IOException 
     {
-        this.jaxbContext = JAXBContext.newInstance(Concept.class);
+//        final File parentFolder = new File("new_xsd/.");
+//        final String schemaName = suggestedSchemaName;
+//        JAXBContext jc = JAXBContext.newInstance(Concept.class);
+//        jc.generateSchema(new SchemaOutputResolver() {
+//            @Override
+//            public Result createOutput(String namespace, String schema) throws IOException
+//            {
+//                return new StreamResult(new File(parentFolder, schemaName));
+//            }
+//        });
+   
     }
-
+    
+    
     public static void main(String args[]) throws JAXBException, IOException
     {
+        
         SchemaOutputResolver sour;
         JAXBContext jaxbContext;
         sour = new PraxiconDBOutputResolver();
         jaxbContext = JAXBContext.newInstance(Concept.class);
+        
         jaxbContext.generateSchema(sour);
-        sour.createOutput("http://www.csri.gr/concept", "output.xsd");
+        //TODO: Fix renaming of xml schema files to ones that make sense
+        //TODO: Now I get a "SimpleNamespaceResolver contained no localNamespaceURI; 
+        //TODO: aborting rename." error.
+        //sour.createOutput("http://www.csri.gr", "output.xsd");
     }
 }
